24bd86d9f1d14fdd4cb57af780419e4cd80a34fe
Sidor007 <Sidor007@yandex.ru>
2023-09-24 13:12:54 +0300

    changesinglePageApplicationLink

--- movies-explorer-frontend/src/components/Constant/Constant.js
+++ movies-explorer-frontend/src/components/Constant/Constant.js
@@ -1,7 +1,7 @@
 module.exports.staticWebSiteLink = "https://sidorsan.github.io/how-to-learn/";
 module.exports.adaptiveWebSiteLink =
   "https://sidorsan.github.io/russian-travel/";
-module.exports.singlePageApplicationLink = "http://sidorsan.nomoredomains.sbs";
+module.exports.singlePageApplicationLink = "https://mesto-1f97c.web.app/";
 module.exports.gitHabLink = "https://github.com/Sidorsan";
 module.exports.yandexPracticumLink = "https://practicum.yandex.ru/";
 module.exports.MovieDurationShotFilm = 40;
